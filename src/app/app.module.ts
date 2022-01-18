ba4d8501495c4c5ad9ab0e374e33163ac997c39b
Manakal <dp.manakal82@gmail.com>
2022-01-18 05:47:43 +0530

    Header Component finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponentComponent } from './components/header-component/header-component.component';
 import { WelcomeImageComponent } from './components/welcome-image/welcome-image.component';
 import { GpaTableComponent } from './components/gpa-table/gpa-table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import { MatTableModule } from '@angular/material/table'
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
 
 
 @NgModule({
@@ -17,9 +28,20 @@ import { GpaTableComponent } from './components/gpa-table/gpa-table.component';
     GpaTableComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
-    DataTablesModule
+    DataTablesModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatDialogModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/gpa-table/gpa-table.component.ts
+++ src/app/components/gpa-table/gpa-table.component.ts
@@ -1,17 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {MatTableDataSource} from "@angular/material/table";
 
+export interface gpa_detail {
+  course: string;
+  grade: string;
+  gpa: number;
+}
+const GPA_DATA: gpa_detail[] = [
+  { course: "Introduction to Programing", grade: "A" ,gpa:4.00},
+  { course: "Object Oriented Programing", grade: "A-" ,gpa:4.00},
+  { course: "Data Structures And Algorithms", grade: "B+" ,gpa:3.30},
+];
+
+/**
+ * @title Basic use of `<table mat-table>`
+ */
 @Component({
   selector: 'app-gpa-table',
   templateUrl: './gpa-table.component.html',
+  styleUrls: ['gpa-table.component.scss'],
 })
 export class GpaTableComponent implements OnInit {
-  dtOptions: DataTables.Settings = {};
-  constructor() { }
+
+  columnsToDisplay: string[] = ["course", "grade","gpa"];
+  dataSource = GPA_DATA;
+  constructor() {
+  }
+
 
   ngOnInit(): void {
-    this.dtOptions = {
-      pagingType: 'full_numbers'
-    };
   }
 
 }
