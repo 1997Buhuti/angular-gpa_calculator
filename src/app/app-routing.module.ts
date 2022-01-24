ed0b2b4aebfc02626aaaeedff664fb8134dc11b2
Manakal <dp.manakal82@gmail.com>
2022-01-24 12:02:56 +0530

    Issue Occured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { FaqComponent } from './components/faq/faq.component';
-
+//import { HomeComponent } from './components/home';
 const routes: Routes = [
   { path: 'faq', component: FaqComponent },
   { path: 'about-us', component: AboutUsComponent },
@@ -13,4 +13,8 @@ const routes: Routes = [
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
-export const routingComponents = [FaqComponent, AboutUsComponent];
+export const routingComponents = [
+  FaqComponent,
+  AboutUsComponent,
+  //HomeComponent,
+];
