3ba6ed99620730bb3966b6271529fc4d15692c1d
Manakal <dp.manakal82@gmail.com>
2022-01-24 12:12:51 +0530

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { FaqComponent } from './components/faq/faq.component';
-//import { HomeComponent } from './components/home';
+import { HomeComponent } from './components/pages/home/home.component';
 const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'faq', component: FaqComponent },
   { path: 'about-us', component: AboutUsComponent },
 ];
@@ -16,5 +17,5 @@ export class AppRoutingModule {}
 export const routingComponents = [
   FaqComponent,
   AboutUsComponent,
-  //HomeComponent,
+  HomeComponent,
 ];
