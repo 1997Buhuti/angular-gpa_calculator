7037497c881023ad18b5c86e7fbcbe872a1faf8a
Manakal <dp.manakal82@gmail.com>
2022-01-24 11:15:30 +0530

    added the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { FaqComponent } from './components/faq/faq.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'faq', component: FaqComponent },
+  { path: 'about-us', component: AboutUsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
+export const routingComponents = [FaqComponent, AboutUsComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,32 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {DataTablesModule} from "angular-datatables";
-import {AppRoutingModule} from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { DataTablesModule } from 'angular-datatables';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 
-import {AppComponent} from './app.component';
-import {HeaderComponentComponent} from './components/header-component/header-component.component';
-import {WelcomeImageComponent} from './components/welcome-image/welcome-image.component';
-import {GpaTableComponent} from './components/gpa-table/gpa-table.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {FormsModule} from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './components/header-component/header-component.component';
+import { WelcomeImageComponent } from './components/welcome-image/welcome-image.component';
+import { GpaTableComponent } from './components/gpa-table/gpa-table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
-import {MatTableModule} from '@angular/material/table'
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
-import {MatCardModule} from '@angular/material/card';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDialogModule} from '@angular/material/dialog';
-import {FooterComponent} from './components/footer/footer.component';
-import {MatIconModule} from "@angular/material/icon";
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatListModule} from "@angular/material/list";
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FooterComponent } from './components/footer/footer.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 import { GpaFormComponent } from './components/gpa-form/gpa-form.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { FinalGpaViewerComponent } from './components/final-gpa-viewer/final-gpa-viewer.component';
+import { FaqComponent } from './components/faq/faq.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 @NgModule({
   declarations: [
@@ -35,6 +37,7 @@ import { FinalGpaViewerComponent } from './components/final-gpa-viewer/final-gpa
     FooterComponent,
     GpaFormComponent,
     FinalGpaViewerComponent,
+    routingComponents,
   ],
   imports: [
     BrowserModule,
@@ -53,11 +56,9 @@ import { FinalGpaViewerComponent } from './components/final-gpa-viewer/final-gpa
     MatIconModule,
     MatSidenavModule,
     MatListModule,
-    HttpClientModule
-
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
