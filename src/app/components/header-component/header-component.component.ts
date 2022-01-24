ebf082c5b8ba76fdfb413f8e6670168968bbc127
Manakal <dp.manakal82@gmail.com>
2022-01-24 07:29:24 +0530

    Navbar Added

--- src/app/components/header-component/header-component.component.ts
+++ src/app/components/header-component/header-component.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatToolbar } from '@angular/material/toolbar';
 @Component({
   selector: 'app-header-component',
   templateUrl: './header-component.component.html',
-  styleUrls: ['./header-component.component.scss']
+  styleUrls: ['./header-component.component.scss'],
 })
 export class HeaderComponentComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
